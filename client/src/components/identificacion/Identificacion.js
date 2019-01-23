afc84b0f1dfdd221f245994c6399dfc48094f0f2
Luis Felipe Flórez <luisfl92@hotmail.com>
2019-01-23 09:28:28 -0500

    estilo mensaje compra 5

--- client/src/components/identificacion/Identificacion.js
+++ client/src/components/identificacion/Identificacion.js
@@ -86,229 +86,221 @@ class Identificacion extends Component {
 
     if (cargando) {
       contenido = (
-        <div className="">
-          <div className="">
-            <img
-              id="fondo_principal"
-              src="../../img/colsanitas_soft-pag_2.jpg"
-              width="748"
-              height="1366"
-              alt=""
-            />
-            <Spinner />
-          </div>
+        <div className="principal">
+          <img
+            id="fondo_principal"
+            src="../../img/colsanitas_soft-pag_2.jpg"
+            width="748"
+            height="1366"
+            alt=""
+          />
+          <Spinner />
         </div>
       );
     } else {
       contenido = (
         <div>
-          <div>
-            <img
-              id="fondo_principal"
-              src="../../img/colsanitas_soft-pag_2.jpg"
-              width="748"
-              height="1366"
-              alt=""
-            />
-            <p id="tdocumento">Escoja su tipo de documento</p>
-            <div className="row text-center" id="checkbox_1">
-              <label
-                id="checkbox"
-                htmlFor="primary"
-                className="btn btn-primary"
-              >
-                Cédula de Ciudadanía{" "}
-                <input
-                  type="radio"
-                  id="primary"
-                  className="badgebox"
-                  value="CC"
-                  checked={this.state.tipoIdentificacion === "CC"}
-                  onChange={this.handleChange}
-                />
-                <span className="badge">&check;</span>
-              </label>
-              <label id="checkbox" htmlFor="second" className="btn btn-primary">
-                Cédula de Extranjería{" "}
-                <input
-                  type="radio"
-                  id="second"
-                  className="badgebox"
-                  value="CE"
-                  checked={this.state.tipoIdentificacion === "CE"}
-                  onChange={this.handleChange}
-                />
-                <span className="badge">&check;</span>
-              </label>
-              <label id="checkbox" htmlFor="third" className="btn btn-primary">
-                Nit{" "}
-                <input
-                  type="checkbox"
-                  id="third"
-                  className="badgebox"
-                  value="NIT"
-                  checked={this.state.tipoIdentificacion === "NIT"}
-                  onChange={this.handleChange}
-                />
-                <span className="badge">&check;</span>
-              </label>
-              {errors.tipoIdentificacion && (
-                <div className="error-message">{errors.tipoIdentificacion}</div>
-              )}
-            </div>
+          <img
+            id="fondo_principal"
+            src="../../img/colsanitas_soft-pag_2.jpg"
+            width="748"
+            height="1366"
+            alt=""
+          />
+          <p id="tdocumento">Escoja su tipo de documento</p>
+          <div className="row text-center" id="checkbox_1">
+            <label id="checkbox" htmlFor="primary" className="btn btn-primary">
+              Cédula de Ciudadanía{" "}
+              <input
+                type="radio"
+                id="primary"
+                className="badgebox"
+                value="CC"
+                checked={this.state.tipoIdentificacion === "CC"}
+                onChange={this.handleChange}
+              />
+              <span className="badge">&#10004;</span>
+            </label>
+            <label id="checkbox" htmlFor="second" className="btn btn-primary">
+              Cédula de Extranjería{" "}
+              <input
+                type="radio"
+                id="second"
+                className="badgebox"
+                value="CE"
+                checked={this.state.tipoIdentificacion === "CE"}
+                onChange={this.handleChange}
+              />
+              <span className="badge">&#10004;</span>
+            </label>
+            <label id="checkbox" htmlFor="third" className="btn btn-primary">
+              Nit{" "}
+              <input
+                type="checkbox"
+                id="third"
+                className="badgebox"
+                value="NIT"
+                checked={this.state.tipoIdentificacion === "NIT"}
+                onChange={this.handleChange}
+              />
+              <span className="badge">&#10004;</span>
+            </label>
+            {errors.tipoIdentificacion && (
+              <div className="error-message">{errors.tipoIdentificacion}</div>
+            )}
+          </div>
 
-            <p id="tcedula">Digite su número de Identificación</p>
-            <img id="imark" src="../../img/input_mark.png" alt="" />
-            <div className="form-group">
-              <form name="login">
-                <label htmlFor="usr">
-                  <input
-                    type="text"
-                    className={classnames("form-control identificacion", {
-                      "is-invalid": errors.numeroIdentificacion
-                    })}
-                    id="usr"
-                    placeholder="Identificación"
-                    name="identificacion"
-                    value={this.state.numeroIdentificacion}
-                    onChange={this.onChange}
-                    onKeyPress={this.onKeyPress}
-                  />
-                </label>
-                {errors.numeroIdentificacion && (
-                  <div className="error-message" id="error-identificacion">
-                    {errors.numeroIdentificacion}
-                  </div>
-                )}
-                {errors.mensaje && (
-                  <div className="error-message" id="error-identificacion">
-                    {errors.mensaje}
-                  </div>
-                )}
-                <div id="keyboard" className="form-group">
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="1"
-                    onClick={this.onClick}
-                  />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="2"
-                    onClick={this.onClick}
-                  />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="3"
-                    onClick={this.onClick}
-                  />
-                  <br />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="4"
-                    onClick={this.onClick}
-                  />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="5"
-                    onClick={this.onClick}
-                  />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="6"
-                    onClick={this.onClick}
-                  />
-                  <br />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="7"
-                    onClick={this.onClick}
-                  />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="8"
-                    onClick={this.onClick}
-                  />
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="9"
-                    onClick={this.onClick}
-                  />
-                  <br />
-                  <button
-                    id="invisible"
-                    style={{
-                      width: "75px",
-                      height: "75px",
-                      marginBottom: "3px",
-                      marginRight: "20px"
-                    }}
-                    type="button"
-                    className="btn btn-primary"
-                  >
-                    0
-                  </button>
-                  <input
-                    style={numeroStyle}
-                    type="button"
-                    className="btn btn-primary"
-                    value="0"
-                    onClick={this.onClick}
-                  />
-                  <button
-                    id="invisible"
-                    style={{
-                      width: "75px",
-                      height: "75px",
-                      marginBottom: "3px"
-                    }}
-                    type="button"
-                    className="btn btn-primary"
-                  >
-                    0
-                  </button>
+          <p id="tcedula">Digite su número de Identificación</p>
+          <img id="imark" src="../../img/input_mark.png" alt="" />
+          <div className="form-group">
+            <form name="login">
+              <label htmlFor="usr">
+                <input
+                  type="text"
+                  className={classnames("form-control identificacion", {
+                    "is-invalid": errors.numeroIdentificacion
+                  })}
+                  id="usr"
+                  placeholder="Identificación"
+                  name="identificacion"
+                  value={this.state.numeroIdentificacion}
+                  onChange={this.onChange}
+                  onKeyPress={this.onKeyPress}
+                />
+              </label>
+              {errors.numeroIdentificacion && (
+                <div className="error-message" id="error-identificacion">
+                  {errors.numeroIdentificacion}
                 </div>
-                <div className="form-group" id="keyboard_2">
-                  <input
-                    style={{
-                      width: "312px",
-                      height: "75px",
-                      marginBottom: "3px",
-                      fontSize: "40px"
-                    }}
-                    type="button"
-                    className="btn btn-primary"
-                    value="ACEPTAR"
-                    onClick={this.onClick}
-                  />
-                  <br />
-                  <input
-                    style={{ width: "312px", height: "75px", fontSize: "40px" }}
-                    type="button"
-                    className="btn btn-primary"
-                    value="BORRAR"
-                    onClick={this.onClick}
-                  />
+              )}
+              {errors.mensaje && (
+                <div className="error-message" id="error-identificacion">
+                  {errors.mensaje}
                 </div>
-              </form>
-            </div>
+              )}
+              <div id="keyboard" className="form-group">
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="1"
+                  onClick={this.onClick}
+                />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="2"
+                  onClick={this.onClick}
+                />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="3"
+                  onClick={this.onClick}
+                />
+                <br />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="4"
+                  onClick={this.onClick}
+                />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="5"
+                  onClick={this.onClick}
+                />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="6"
+                  onClick={this.onClick}
+                />
+                <br />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="7"
+                  onClick={this.onClick}
+                />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="8"
+                  onClick={this.onClick}
+                />
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="9"
+                  onClick={this.onClick}
+                />
+                <br />
+                <button
+                  id="invisible"
+                  style={{
+                    width: "75px",
+                    height: "75px",
+                    marginBottom: "3px",
+                    marginRight: "20px"
+                  }}
+                  type="button"
+                  className="btn btn-primary"
+                >
+                  0
+                </button>
+                <input
+                  style={numeroStyle}
+                  type="button"
+                  className="btn btn-primary"
+                  value="0"
+                  onClick={this.onClick}
+                />
+                <button
+                  id="invisible"
+                  style={{
+                    width: "75px",
+                    height: "75px",
+                    marginBottom: "3px"
+                  }}
+                  type="button"
+                  className="btn btn-primary"
+                >
+                  0
+                </button>
+              </div>
+              <div className="form-group" id="keyboard_2">
+                <input
+                  style={{
+                    width: "312px",
+                    height: "75px",
+                    marginBottom: "3px",
+                    fontSize: "40px"
+                  }}
+                  type="button"
+                  className="btn btn-primary"
+                  value="ACEPTAR"
+                  onClick={this.onClick}
+                />
+                <br />
+                <input
+                  style={{ width: "312px", height: "75px", fontSize: "40px" }}
+                  type="button"
+                  className="btn btn-primary"
+                  value="BORRAR"
+                  onClick={this.onClick}
+                />
+              </div>
+            </form>
           </div>
         </div>
       );
